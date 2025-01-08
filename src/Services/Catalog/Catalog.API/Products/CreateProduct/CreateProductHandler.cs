59fc071c0854f13413b8acfdf9cab1e84330a949
Mykyta Deyneha <mykytadeyneha@icloud.com>
2025-01-08 12:59:02 +0000

    Vertical Design Init for catalog API

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(
+    string Name, 
+    List<string> Category, 
+    string description, 
+    string imageFile, 
+    decimal price
+)
+: IRequest<CreateProductResult>;
+
+public record CreateProductResult(
+    Guid id
+);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // Business logic to create a product
+        throw new NotImplementedException();
+    }
+}
