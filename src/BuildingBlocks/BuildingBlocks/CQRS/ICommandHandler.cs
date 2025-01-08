8a46c1189ba9c26fd14ae0aa2d37ce5b58f9d30c
Mykyta Deyneha <mykytadeyneha@icloud.com>
2025-01-08 14:47:35 +0000

    init CQRS interfaces

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommandHandler<in TComand> : IRequestHandler<TComand, Unit>
+    where TComand : ICommand<Unit>
+{
+
+}
+
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse :  notnull
+{
+
+}
