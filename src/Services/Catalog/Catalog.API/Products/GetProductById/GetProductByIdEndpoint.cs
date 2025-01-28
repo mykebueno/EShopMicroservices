d0f4c9330267cd27f9733e0e6702f3e524cff7f5
Mykyta Deyneha <mykytadeyneha@icloud.com>
2025-01-28 16:35:27 +0000

    GetProductById endpoint implmentation

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdRequest();
+
+public record GetProductByIdResponse( Product Product );
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            GetProductByIdResult result = await sender.Send(new GetProductByIdQuery(id));
+
+            GetProductByIdResponse response = result.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductsById")
+        .Produces<GetProductsResposne>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products by Id")
+        .WithDescription("Get Products by Id"); 
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,28 @@
+﻿namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsRequest();
+
+public record GetProductsResposne(
+        IEnumerable<Product> Products
+    );
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app
+            .MapGet("products", async (ISender sender) =>
+            {
+                GetProductsResult result = await sender.Send(new GetProductsQuery());
+
+                GetProductsResposne response = result.Adapt<GetProductsResposne>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResposne>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+    }
+}
