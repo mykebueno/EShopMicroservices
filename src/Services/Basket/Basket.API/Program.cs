c77d5b723e1734ec91964230375a79d8f3b6e48a
Mykyta Deyneha <mykytadeyneha@icloud.com>
2025-05-20 22:34:44 +0100

    configure mediatr and carter on program

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,12 +1,23 @@
 using Basket.API.Carter;
+using BuildingBlocks.Behaviours;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var programAssembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(programAssembly);
+    config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+    config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+});
+builder.Services.AddValidatorsFromAssembly(programAssembly);
+
 builder.Services.AddCarter(new DependencyContextAssemblyCatalogCustom());
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
