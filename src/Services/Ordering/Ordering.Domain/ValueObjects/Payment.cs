ca3f946a9194030f9960106bef2f87b3b443b138
Mykyta Deyneha <mykytadeyneha@icloud.com>
2025-07-24 23:37:24 +0100

    Init Domain

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record Payment
+{
+    public string CardNumber { get; init; } = default!;
+    public string? CardName { get; init; } = default!;
+    public string Expiration { get; init; } = default!;
+    public string CVV { get; init; } = default!;
+    public int PaymentMethod { get; } = default!;
+}
