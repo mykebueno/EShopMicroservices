a2f9ac1394a75c7df7a00d78ca6a80bda09e61ce
Mykyta Deyneha <mykytadeyneha@icloud.com>
2025-07-29 22:57:17 +0100

    Implementing Interceptors

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Application;
 
@@ -6,8 +7,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        // Register application services here
-        // Example: services.AddScoped<IOrderService, OrderService>();
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
         return services;
     }  
 }
